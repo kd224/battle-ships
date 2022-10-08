8d2a58b5c9cb3ef3c46a7f75697388b1a5dd59c5
kd224 <kd224mc@gmail.com>
2022-10-08 23:01:32 +0200

    feat: Add labels for grid

---
+++ lib/utils/alphabet_notation.dart
@@ -0,0 +1,2 @@
+// Used to label grid squares.
+const alphabetNotation = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
--- lib/widgets/grid_painter.dart
+++ lib/widgets/grid_painter.dart
@@ -1,6 +1,25 @@
+import 'package:battle_ships/utils/alphabet_notation.dart';
 import 'package:flutter/material.dart';
 
 class MyGridPainter extends CustomPainter {
+  TextPainter _getNotationText(String text) {
+    TextSpan span = TextSpan(
+      style: const TextStyle(
+        color: Colors.tealAccent,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      text: text,
+    );
+    TextPainter tp = TextPainter(
+      text: span,
+      textAlign: TextAlign.end,
+      textDirection: TextDirection.ltr,
+    );
+
+    return tp;
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
     final Paint linePaint = Paint()..color = Colors.grey[500]!;
@@ -12,6 +31,16 @@ class MyGridPainter extends CustomPainter {
 
       // Horizontal lines.
       canvas.drawLine(Offset(500, i * 50), Offset(0, i * 50), linePaint);
+
+      if (i < 10) {
+        final numbers = _getNotationText((i + 1).toInt().toString());
+        numbers.layout();
+        numbers.paint(canvas, Offset(-30, i * 50 + 15));
+
+        final letters = _getNotationText(alphabetNotation[i.toInt()]);
+        letters.layout();
+        letters.paint(canvas, Offset(i * 50 + 20, -30));
+      }
     }
   }
 
